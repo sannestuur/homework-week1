034bd9295d79d6bf8faa374eed91efd79b2a8ac7
Sanne Stuur <sanne.stuur@gmail.com>
2018-02-17 21:07:44 +0100

    added method for mapping names and phones from contacts (assignment 1)

--- main.rb
+++ main.rb
@@ -4,3 +4,20 @@ def read_contacts
   contacts_json = File.read("contacts.json")
   return JSON.parse(contacts_json, { symbolize_names: true })
 end
+
+
+
+#This method maps the array with all contact information for all contacts (stored in contacts.json), to an array of hashes containing only the contact's name and the contact's phone number (Assignment 1)
+def map_contacts(contacts)
+  contacts_phone = contacts.map{|contact|
+  {contact[:name] => contact[:phone]}
+  }
+
+  print contacts_phone
+end
+
+
+
+contacts = read_contacts
+
+map_contacts(contacts)
